4eae74039d400975c8b5412b2bcb690b12442e55
pranav21999 <pranavdani99@gmail.com>
2021-06-25 00:38:21 +0530

    method for edit contacts in AddressBook is added

---
+++ AddressBookMain.java
@@ -0,0 +1,162 @@
+package addressbooksystem;
+import java.util.*;
+
+
+public class AddressBookMain{
+
+	
+    static Scanner sc = new Scanner(System.in);
+    static ArrayList<ContactItems> contactList = new ArrayList<>();
+    // Main method
+    public static void main(String[] args) 
+    {
+
+        System.out.println("Welcome to the Address book program"); // Welcome statement
+        AddressBookMain addressBookMain = new AddressBookMain();
+        addressBookMain.addContactList();
+    }
+    
+    public void display(ArrayList<ContactItems> contactList)//Display Address book
+   {
+        for (ContactItems contactItems : contactList) 
+        {
+            System.out.println(contactItems);
+        }
+    }
+    
+    //method to add new contacts in addressbook
+    public void addContact(ContactItems contactItems, ArrayList<ContactItems> contactList) {
+
+        contactItems = new ContactItems();
+
+        System.out.println("Enter first name : ");
+        contactItems.firstName = sc.next();
+
+        System.out.println("Enter last name : ");
+        contactItems.lastName = sc.next();
+
+        System.out.println("Enter address : ");
+        contactItems.address = sc.next();
+       
+
+        System.out.println("Enter city : ");
+        contactItems.city = sc.next();
+
+        System.out.println("Enter state : ");
+        contactItems.state = sc.next();
+
+        System.out.println("Enter zip code : ");
+        contactItems.zip = sc.nextInt();
+
+        System.out.println("Enter phone number : ");
+        contactItems.phoneNumber = sc.nextLong();
+
+        System.out.println("Enter email : ");
+        contactItems.email = sc.next();
+
+        contactList.add(contactItems);
+
+    }
+
+	//method to edit contact in contact book
+    public void editContact() 
+	{
+
+        System.out.println("Enter the first name of contact you wish to edit");
+        String edit = sc.nextLine();
+        System.out.println("Press 1 - To edit first name");
+        System.out.println("Press 2 - To edit last name");
+        System.out.println("Press 3 - To edit address");
+        System.out.println("Press 4 - To edit city");
+        System.out.println("Press 5 - To edit state");
+        System.out.println("Press 6 - To edit zip code");
+        System.out.println("Press 7 - To edit phone number");
+        System.out.println("Press 8 - To edit email");
+        int choice = sc.nextInt();
+        if (choice == 1) {
+            for (ContactItems contactItems : contactList) {
+                if (contactItems.firstName.equals(edit)) {
+                    System.out.println("Enter new first name");
+                    contactItems.firstName = sc.next();
+                }
+            }
+        }
+        if (choice == 2) {
+            for (ContactItems contactItems : contactList) {
+                if (contactItems.firstName.equals(edit)) {
+                    System.out.println("Enter new last name");
+                    contactItems.lastName = sc.next();
+                }
+            }
+        }
+        if (choice == 3) {
+            for (ContactItems contactItems : contactList) {
+                if (contactItems.firstName.equals(edit)) {
+                    System.out.println("Enter new address");
+                    contactItems.address = sc.next();
+                }
+            }
+        }
+        if (choice == 4) {
+            for (ContactItems contactItems : contactList) {
+                if (contactItems.firstName.equals(edit)) {
+                    System.out.println("Enter new city");
+                    contactItems.city = sc.next();
+                }
+            }
+        }
+        if (choice == 5) {
+            for (ContactItems contactItems : contactList) {
+                if (contactItems.firstName.equals(edit)) {
+                    System.out.println("Enter new state");
+                    contactItems.state = sc.next();
+                }
+            }
+        }
+        if (choice == 6) {
+            for (ContactItems contactItems : contactList) {
+                if (contactItems.firstName.equals(edit)) {
+                    System.out.println("Enter new zip code");
+                    contactItems.zip = sc.nextInt();
+                }
+            }
+        }
+        if (choice == 7) {
+            for (ContactItems contactItems : contactList) {
+                if (contactItems.firstName.equals(edit)) {
+                    System.out.println("Enter new phone number");
+                    contactItems.phoneNumber = sc.nextLong();
+                }
+            }
+        }
+        if (choice == 8) {
+            for (ContactItems contactItems : contactList) {
+                if (contactItems.firstName.equals(edit)) {
+                    System.out.println("Enter new email");
+                    contactItems.email = sc.next();
+                }
+            }
+        }
+    }
+
+ 	// method to show conatctList in addressBook
+      public void addContactList() {
+        while (true) {
+            System.out.println("Press 0 - Display all contacts");
+            System.out.println("Press 1 - Add contact");
+            System.out.println("Press 2 - Edit contact");
+            System.out.println("Press 6 - Exit");
+            int option = sc.nextInt();
+            sc.nextLine();
+
+            switch (option) {
+                case 0 -> display(contactList);
+                case 1 -> addContact(null, contactList);
+                case 2 -> editContact();
+            }
+            if (option == 6) {
+                break;
+            }
+        }
+    }
+}
