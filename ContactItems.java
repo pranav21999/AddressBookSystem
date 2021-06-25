6fe115d1633712af72ef446ed95cb1b2ebfda058
pranav21999 <pranavdani99@gmail.com>
2021-06-25 08:42:56 +0530

    ContactItems.java added

---
+++ ContactItems.java
@@ -0,0 +1,22 @@
+package addressbooksystem;
+
+public class ContactItems {
+
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    int zip;
+    long phoneNumber;
+    String email;
+	
+	
+	public String toString() {
+		return "ContactItems [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city="
+				+ city + ", state=" + state + ", zip=" + zip + ", phoneNumber=" + phoneNumber + ", email=" + email
+				+ "]";
+	}
+	
+	
+}
